6fc852345c7dc7bba479add041cfe2a9092b0de2
janismdhanbad <janpreet@umich.edu>
2022-12-04 18:36:40 -0500

    push changes

--- js/main_file.js
+++ js/main_file.js
@@ -64,15 +64,45 @@ function showProj4() {
 }
     
 
-function getReadme() {
-    fetch(`https://raw.githubusercontent.com/janismdhanbad/portfolio-content/master/README.md`) // Fetch the file from GitHub's api
-        .then(response => response.json())
-        .then(data => {
-            const content = atob(data.content); // Convert from base64 to readable text
-                  document.getElementById("readme-text").textContent = content; // Apply content to the document
-            console.log(content); // Log the content to the console
-        })
-        .catch(error => console.log(error)); // Catch any errors
-}
+// function getReadme() {
+//     fetch(`https://raw.githubusercontent.com/janismdhanbad/portfolio-content/master/README.md`) // Fetch the file from GitHub's api
+//         .then(response => response.json())
+//         .then(data => {
+//             const content = atob(data.content); // Convert from base64 to readable text
+//                   document.getElementById("readme-text").textContent = content; // Apply content to the document
+//             console.log(content); // Log the content to the console
+//         })
+//         .catch(error => console.log(error)); // Catch any errors
+// }
 
-getReadme();
+// getReadme();
+
+(function () {
+    var file = "../blogs_md/Hello-world.md";
+    var reader = new stmd.DocParser();
+    var writer = new stmd.HtmlRenderer();
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+      if(xhr.readyState === 4 && xhr.status === 200) {
+        display(xhr);
+      }
+    };
+  
+    function display(xhr) {
+      var parsed = reader.parse(xhr.responseText);
+      var content = writer.renderBlock(parsed);
+      document.getElementsByTagName('body')[0].innerHTML = content;
+      
+      /* try to extract h1 title and use as title for page
+         if no h1, use name of file 
+      */
+      try {
+        document.title = document.querySelector('h1').textContent
+      } catch (e) {
+        document.title = file;
+      }
+    }
+  
+    xhr.open('GET', file);
+    xhr.send();
+  })();
